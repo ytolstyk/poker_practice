fee633e5da9558fe5483236fae0670535d5bb07b
= <=>
2014-08-15 15:19:27 -0700

    pair and high cards logic

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -9,4 +9,74 @@ class Hand
     @cards = deck.draw(5)
   end
   
+  def type
+    hands = [straight_flush, four_kind, full_house, flush, straight,
+      three_kind, two_pair, pair, high_cards]
+    
+    hands.each do |method|
+      return method if method
+    end
+  end
+  
+  def straight_flush
+  end
+  
+  def four_kind
+  end
+  
+  def full_house
+  end
+  
+  def flush
+  end
+  
+  def straight
+  end
+  
+  def three_kind
+    if values_count.values.count == 3 && values_count.values.max == 3
+  end
+  
+  def two_pair
+    if values_count.values.count == 3
+      results = [:two_pair]
+      
+    end
+    
+    false
+  end
+  
+  def pair
+    if values_count.values.count == 4
+      results = [:pair]
+      cards_remaining = values_count.keys
+      values_count.each do |k, v|
+        if v == 2
+          results << k 
+          cards_remaining.delete(k)
+        end
+      end
+      return results + cards_remaining.sort.reverse.take(1)
+    end
+    
+    false
+  end
+  
+  def high_cards
+    result = card_values.sort.reverse.take(2)
+    result.unshift(:high_card)
+  end
+  
+  def values_count
+    counts = Hash.new { |h, k| h[k] = 0 }
+    card_values.each do |value|
+      counts[value] += 1
+    end
+    counts
+  end
+  
+  def card_values
+    cards.map { |card| card.value }
+  end
+  
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -10,118 +10,118 @@ RSpec.describe Hand do
   
   describe "#type" do
     it "correctly identifies a straight flush" do
-      let(:hand) { Hand.new(Deck.new) }
-      hand.cards = [
+      straight_flush =  Hand.new(Deck.new) 
+      straight_flush.cards = [
         Card.new(2, :hearts),
         Card.new(3, :hearts),
         Card.new(4, :hearts),
         Card.new(5, :hearts),
         Card.new(6, :hearts)
-                  ]
+      ]
       
-      expect(hand.type).to eq([:straight_flush, 6, 6])
+      expect(straight_flush.type).to eq([:straight_flush, 6, 6])
     end
     
     it "correctly identifies four of a kind" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(5, :hearts),
         Card.new(5, :spades),
         Card.new(5, :clubs),
         Card.new(5, :diamonds),
         Card.new(6, :clubs)
-                  ]
+      ]
       
       expect(hand.type).to eq([:four_kind, 5, 6])
     end
     
     it "correctly identifies a full house" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(3, :hearts),
         Card.new(3, :clubs),
         Card.new(3, :spades),
         Card.new(5, :diamonds),
         Card.new(5, :hearts)
-                  ]
+      ]
       
       expect(hand.type).to eq([:full_house, 3, 5])
     end
     
     it "correctly identifies a flush" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(2, :hearts),
         Card.new(7, :hearts),
         Card.new(4, :hearts),
         Card.new(12, :hearts),
         Card.new(6, :hearts)
-                  ]
+      ]
       
       expect(hand.type).to eq([:flush, 12, 7])
     end
     
     it "correctly identifies a straight" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(2, :hearts),
         Card.new(3, :clubs),
         Card.new(4, :hearts),
         Card.new(5, :diamonds),
         Card.new(6, :clubs)
-                  ]
+      ]
       
       expect(hand.type).to eq([:straight, 6, 6])
     end
     
     it "correctly identifies three of a kind" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(3, :hearts),
         Card.new(3, :clubs),
         Card.new(3, :diamonds),
         Card.new(5, :hearts),
         Card.new(6, :hearts)
-                  ]
+      ]
       
       expect(hand.type).to eq([:three_kind, 3, 6])
     end
   
     it "correctly identifies two pair" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(2, :hearts),
         Card.new(2, :clubs),
         Card.new(4, :hearts),
         Card.new(5, :hearts),
         Card.new(5, :diamonds)
-                  ]
+      ]
       
       expect(hand.type).to eq([:two_pair, 5, 2])
     end
     
     it "correctly identifies one pair" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(2, :hearts),
         Card.new(2, :clubs),
         Card.new(4, :hearts),
         Card.new(5, :diamonds),
         Card.new(6, :hearts)
-                  ]
+      ]
       
       expect(hand.type).to eq([:pair, 2, 6])
     end
     
     it "correctly identifies a high card" do
-      let(:hand) { Hand.new(Deck.new) }
+      hand = Hand.new(Deck.new)
       hand.cards = [
         Card.new(2, :hearts),
         Card.new(3, :clubs),
         Card.new(7, :hearts),
         Card.new(5, :hearts),
         Card.new(12, :hearts)
-                  ]
+      ]
       
       expect(hand.type).to eq([:high_card, 12, 7])
     end
